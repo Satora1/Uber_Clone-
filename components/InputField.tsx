b666186a5666117f73cf32bb21d30bf9cfddbce5
Jakub Satora <kubasatora@wp.pl>
2024-09-18 19:20:31 +0200

    sign up page

---
+++ components/InputField.tsx
@@ -0,0 +1,11 @@
+import { KeyboardAvoidingView } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+
+const InputField = () => (
+    <KeyboardAvoidingView>
+        <TouchableWithoutFeedback>
+            
+        </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+)
+export default InputField;
