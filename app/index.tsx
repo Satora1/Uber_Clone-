d6c9396c6b04549c3400a7295066e0b2d107ec1a
Jakub Satora <kubasatora@wp.pl>
2024-09-23 21:56:21 +0200

    register page and email verfication

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,16 @@
+import { useAuth } from "@clerk/clerk-expo"
 import { Redirect } from "expo-router"
 
 
 const Home = () => {
+    const { isSignedIn } = useAuth()
+
+    if (isSignedIn) {
+        return <Redirect href={'/(root)/(tabs)/home'} />
+    }
     return <Redirect href="/(auth)/welcome/"></Redirect>
 
-    
+
 }
 
 export default Home;
