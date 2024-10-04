2266cf833c47921604f8c4514d221269cc8faf4a
Jakub Satora <kubasatora@wp.pl>
2024-10-04 15:00:58 +0200

    implemented google maps

--- components/map.tsx
+++ components/map.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import { Text, View } from 'react-native'
-
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps"
 const Map = () => {
-  return (
-   <View>
-    <Text>
-        Map
-    </Text>
-   </View>
-  )
+    const region = {
+
+    }
+    return (
+        <MapView
+            provider={PROVIDER_DEFAULT}
+            className='w-full h-full rounded-2xl'
+            tintColor='black'
+            mapType='mutedStandard'
+            showsPointsOfInterest={false}
+            //initialRegion={region}
+            showsUserLocation={true}
+            userInterfaceStyle='light'
+        >
+            <Text>
+                Map
+            </Text>
+        </MapView>
+    )
 }
 
 export default Map
