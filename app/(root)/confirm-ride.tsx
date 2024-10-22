c24c50b510a7d0294af7dc416ea22ea8fd0b769a
Jakub Satora <kubasatora@wp.pl>
2024-10-22 11:49:42 +0200

    confirm ride

---
+++ app/(root)/confirm-ride.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+
+const ConfirmRide=()=>{
+return(
+    <View>
+        <Text>
+            Confirm ride
+        </Text>
+    </View>
+)
+}
+export default ConfirmRide
